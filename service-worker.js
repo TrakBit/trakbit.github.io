f34ec1403a47fa3db8fc245de72a0ae33a2c2df7
trakbit <harsh.vardhan.611@gmail.com>
2018-11-08 15:48:55 +0530

    rebranding

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.48dd963f8b3807a306badb9c857f2cb7.js"
+  "./precache-manifest.b061ae86f6ede0a1c6354eb0641ed575.js"
 );
 
 workbox.clientsClaim();
